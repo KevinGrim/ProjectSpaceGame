fc101364067c4bbdc11c8d2bbf239ad7e0c18897
Kevin Grim <Kevin.schweisfurth@t-online.de>
2025-04-04 11:37:03 +0200

    New Space Ship Import, New Firemode + UI vorbereitung

--- ProjectNebulon/Assets/Scripts/RayShoot.cs
+++ ProjectNebulon/Assets/Scripts/RayShoot.cs
@@ -7,16 +7,20 @@ public class RayShoot : MonoBehaviour
 
     public float damage = 5f;
     public float range = 120f;
+    public float fireRate = 15f;
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
 
+    private float nextTimeToFire = 0f;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
             Shoot();
+            nextTimeToFire = Time.time + 1f / fireRate;
         }
     }
 
